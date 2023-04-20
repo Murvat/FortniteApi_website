71981cf4ae820bdee518fbec6e5b540208cbf549
Murvat <murvat.mustafayev@mail.ru>
2023-04-20 16:50:51 +0400

    NEW BasketItem

---
+++ src/components/BasketItem.jsx
@@ -0,0 +1,15 @@
+function BasketItem {
+    const {
+        id,
+        name,
+        price,
+        quantity,
+    } = props;
+return(
+    <li href="#!" className="collection-item ">
+        {name} x {quantity} = {price}
+        <span href="#!" class="secondary-content">
+            <i class="material-icons">delete_forever</i></span></li>
+)
+};
+export {BasketItem}
